a4bbc259fe2a24ea3b7141452b5a0108cc97a0e9
cxq257990 <83743149+cxq257990@users.noreply.github.com>
2021-07-26 15:41:45 +0800

    Create zhengxin-小程序-common-js726.vue

---
+++ zhengxin-小程序-common-js726.vue
@@ -0,0 +1,24 @@
+
+回调
+onToggleIDSelect() {
+				let fn = async () => {
+					this.showIDSelect = !this.showIDSelect;
+					if (this.showIDSelect || this.showEmoji) {
+						this.showBg = true;
+					} else {
+						this.showBg = false;
+					}
+
+				};
+				this.$util.checkIslogin(fn.bind(null));
+			},
+  
+  	checkIslogin: (fn) => {
+		if (!!uni.getStorageSync('userPhone')) {
+			fn();
+		} else {
+			uni.navigateTo({
+				url: '/pages/login/login'
+			});
+		}
+	},
